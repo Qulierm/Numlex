3a625036321de5efff38a8c6900492705e2df47f
Qulierm <nikita.gostevsky@gmail.com>
2024-06-28 22:53:44 +0300

    New code

--- src/settings.js
+++ src/settings.js
@@ -7,16 +7,14 @@ function closeSettings() {
 }
 
 function saveSettings() {
-	const colorSelector = document.getElementById('colorSelector')
-	const selectedColor = colorSelector.value
 	const fontSelector = document.getElementById('fontSelector')
 	const selectedFont = fontSelector.value
 
 	const textarea = document.getElementById('input')
 	const pre = document.getElementById('code')
-	textarea.className = '' // Clear all current classes
+	editor.className = '' // Clear all current classes
 	pre.className = '' // Clear all current classes
 	textarea.classList.add(selectedColor) // Add selected class
 	pre.classList.add(selectedFont)
-	textarea.classList.add(selectedFont)
+	editor.classList.add(selectedFont)
 }
