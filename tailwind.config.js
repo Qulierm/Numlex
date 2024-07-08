088beb5acbd0a3ea0abca8748f57f3da7c55a49a
Qulierm <nikita.gostevsky@gmail.com>
2024-07-08 21:24:21 +0300

    New code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
 
